130b202cfb060e07bee3412cea7ae845b67fc7db
Julien Chiquet <julien.chiquet@gmail.com>
2018-05-02 22:22:22 +0200

    sticker first version

---
+++ create_sticker.R
@@ -0,0 +1,9 @@
+library(hexSticker)
+imgurl <- "lighthouse.png"
+p1 <- sticker(imgurl, package="State Of The R", p_size = 18, s_x = 1, s_y = .8, s_width = .4, 
+        h_color="#a6bddb", h_fill="#2b8cbe", h_size = 1.5, p_color = "#ece7f2",
+        filename="sotr_blue.png", url = "https://stateofther.github.io", u_size = 5.8, u_color = "#ece7f2")
+
+p2 <- sticker(imgurl, package="State Of The R", p_size = 18, s_x = 1, s_y = .8, s_width = .4, 
+        h_color="#fc9272", h_fill="#de2d26", h_size = 1.5, p_color = "#fee0d2",
+        filename="sotr_red", url = "https://stateofther.github.io", u_size = 5.8, u_color = "#fee0d2")
