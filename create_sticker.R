bf0c140d42a3c5bf32ef65bb04dd37f6740b8a84
Aubert Julie <julie.aubert@agroparistech.fr>
2026-08-20 15:11:48 +0200

    Add files via upload
    New sticker without url and with palette created from photo (point Perharidy)

--- create_sticker.R
+++ create_sticker.R
@@ -1,4 +1,6 @@
 library(hexSticker)
+# palette creee à partir de photo issue du site office de tourisme pointe de Perharidy
+# pale_perharidy <- c(#3B90B0, #A7BCC2, #023E4E, #737D6F, #004A5A, #4C6C72, #9ACFED, #00647D)
 
 imgurl <- "lighthouse.png"
 
@@ -6,6 +8,12 @@ p1 <- sticker(imgurl, package="State Of The R", p_size = 18, s_x = 1, s_y = .8, 
         h_color="#a6bddb", h_fill="#2b8cbe", h_size = 1.5, p_color = "#ece7f2",
         filename="sotr_blue.png", url = "https://stateofther.github.io", u_size = 5.8, u_color = "#ece7f2")
 
+p1_sansurl_perharidy <- sticker(imgurl, package="StateOfTheR", p_size = 18, s_x = 1, s_y = .8, s_width = .4, 
+                                                                 h_color="#9ACFED", h_fill="#00647D", h_size = 3, p_color = "#A7BCC2",
+                                                                 filename="sotr_perharidy.png", url = "", u_size = 5.8, u_color = "#A7BCC2")
+# verif vision
+# library(colorblindr)
+# cvd_grid(p1_sansurl_perharidy)
 p2 <- sticker(imgurl, package="State Of The R", p_size = 18, s_x = 1, s_y = .8, s_width = .4, 
         h_color="#fc9272", h_fill="#de2d26", h_size = 1.5, p_color = "#fee0d2",
         filename="sotr_red", url = "https://stateofther.github.io", u_size = 5.8, u_color = "#fee0d2")
